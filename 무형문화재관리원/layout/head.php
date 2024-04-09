127e8669d5da5894b512164a2b5d89fee9d67d5b
sehoon2510 <sehoon2510@gmail.com>
2024-04-09 17:24:45 +0900

    무형문화재관리원

---
+++ 무형문화재관리원/layout/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style src="../common/fontawesome/css/font-awesome.css"></style>
+    <style src="../common/css/style.css"></style>
+    <link rel="stylesheet" href="../lib/jquery-3.4.1.min.js">
+</head>
